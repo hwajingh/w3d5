10dcbb785f07009b3e4fdcc460a98fc1c6dd163b
hwajingh <hjhong0930@gmail.com>
2022-02-25 10:30:14 -0500

    commit

--- exercises.py
+++ exercises.py
@@ -1,6 +1,9 @@
 
-
+# exercise 2 
 author = ["Joel Carter", "Victor aNisimov", "Andrew P. Garfield","David hassELHOFF","Gary A.J. Bernstein"]
 x = sorted(author, key=lambda name: name.split(" ")[-1].lower())
 print (x)
 print (author)
+
+# exercise 3
+# cannot figure this out
